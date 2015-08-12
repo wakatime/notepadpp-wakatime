c3d2335e5f0b91831cf1c8f9e3eeae3166d2ec02
Carlos Henrique Gandarez <gandarez@gmail.com>
2015-08-12 00:05:09 -0300

    Following standards defined

--- WakaTime/Logger.cs
+++ WakaTime/Logger.cs
@@ -3,39 +3,47 @@ using System.IO;
 
 namespace WakaTime
 {
-    class Logger
+    internal enum LogLevel
+    {
+        Debug = 1,
+        Info,
+        Warning,
+        HandledException
+    };
+
+    static class Logger
     {        
         internal static void Debug(string msg)
         {
             if (!WakaTime.Debug)
                 return;
 
-            Log("Debug", msg);
+            Log(LogLevel.Debug, msg);
         }
 
         internal static void Info(string msg)
         {
-            Log("Info", msg);
+            Log(LogLevel.Info, msg);
         }
 
         internal static void Warning(string msg)
         {
-            Log("Warning", msg);
+            Log(LogLevel.Warning, msg);
         }
 
         internal static void Error(string msg, Exception ex = null)
         {
             var exceptionMessage = string.Format("{0}: {1}", msg, ex);
 
-            Log("Handled Exception", exceptionMessage);
+            Log(LogLevel.HandledException, exceptionMessage);
         }
 
-        internal static void Log(string level, string msg)
+        internal static void Log(LogLevel level, string msg)
         {
             var writer = Setup();
             if (writer == null) return;
 
-            writer.WriteLine("[Wakatime {0} {1}] {2}", level, DateTime.Now.ToString("hh:mm:ss tt"), msg);            
+            writer.WriteLine("[Wakatime {0} {1}] {2}", Enum.GetName(level.GetType(), level), DateTime.Now.ToString("hh:mm:ss tt"), msg);            
             writer.Flush();
             writer.Close();
         }
